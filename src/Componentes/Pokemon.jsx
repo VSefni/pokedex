2a6a85c31da45d2ed770662ef68b046a9c90a085
Adrián Moral <119487999+VSefni@users.noreply.github.com>
2026-02-10 09:51:22 +0100

    Improve error handling and display in Pokemon component

--- src/Componentes/Pokemon.jsx
+++ src/Componentes/Pokemon.jsx
@@ -34,8 +34,14 @@ function Pokemon() {
     function cambioBusqueda(valor) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${valor}`)
             .then(res => res.json())
-            .then(data => {setNumero(data.id); setError("")})
-            .catch(e => setError("Pokemon no existente"))
+            .then(data => {
+                setNumero(data.id);
+                setError("")
+            })
+            .catch(e => {
+                setError("Pokemon no existente");
+                console.log(e)
+            })
     }
 
     if (!pokemon) {
@@ -50,9 +56,9 @@ function Pokemon() {
                 <fieldset>
                     <label htmlFor="Buscador">Nombre: </label>
                     <input id="Buscador" type="text"/>
-                    <label htmlFor="Buscador"> {error}</label>
+                    <label htmlFor="Buscador"> <button>Buscar</button></label>
+                    <label htmlFor="Buscador" className="error"> {error}</label>
                 </fieldset>
-                <button>Buscar</button>
             </form>
             <div className={"Tarjeta " + pokemon.types[0].type.name}>
                 <div className="informacion">
@@ -60,13 +66,20 @@ function Pokemon() {
                     <img src={pokemon.sprites.other["official-artwork"].front_default} alt="pokemon"/>
                 </div>
                 <div className="estadisticas">
+                    <p><strong>Habilidad 1</strong>: {pokemon.abilities[0].ability.name}</p>
+                    <p><strong>Habilidad
+                        2</strong>: {pokemon.abilities[1] ? pokemon.abilities[1].ability.name : "No tiene"}</p>
+                    <p><strong>Habilidad
+                        3</strong>: {pokemon.abilities[2] ? pokemon.abilities[2].ability.name : "No tiene"}</p>
                     <p><strong>HP</strong>: {pokemon.stats[0].base_stat}</p>
                     <p><strong>Ataque</strong>: {pokemon.stats[1].base_stat}</p>
                     <p><strong>Defensa</strong>: {pokemon.stats[2].base_stat}</p>
                     <p><strong>Ataque Especial</strong>: {pokemon.stats[3].base_stat}</p>
                     <p><strong>Defensa Especial</strong>: {pokemon.stats[4].base_stat}</p>
                     <p><strong>Velocidad</strong>: {pokemon.stats[5].base_stat}</p>
-                    <p><strong>Base Stats</strong>: {pokemon.stats[0].base_stat+pokemon.stats[1].base_stat+pokemon.stats[2].base_stat+pokemon.stats[3].base_stat+pokemon.stats[4].base_stat+pokemon.stats[5].base_stat}</p>
+                    <p>
+                        <strong>Total</strong>: {pokemon.stats[0].base_stat + pokemon.stats[1].base_stat + pokemon.stats[2].base_stat + pokemon.stats[3].base_stat + pokemon.stats[4].base_stat + pokemon.stats[5].base_stat}
+                    </p>
                 </div>
             </div>
             <div className="Botones">
